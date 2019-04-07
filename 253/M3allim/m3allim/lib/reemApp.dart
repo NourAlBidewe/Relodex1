fd4db4c5570832f7a99d3d20d2014b38880022a0
issa1618 <49068288+issa1618@users.noreply.github.com>
2019-04-07 22:14:33 +0300

    Update reemApp.dart

--- 253/M3allim/m3allim/lib/reemApp.dart
+++ 253/M3allim/m3allim/lib/reemApp.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:m3allim/bottomNavigator.dart';
-import 'package:m3allim/serviceProvider.dart';
+import 'package:m3allim/Servicer.dart';
 import './Categories.dart';
 import './designCard.dart';
 import './test.dart';
@@ -47,11 +47,11 @@ import 'Doha.dart';
                                   builder: (context) => card(c[c.keys.elementAt(index)])));
                         }
                         else{
-  var reem = serviceProvider('راس بيروت', 29, 3.0, [1,5,10], "xx", true, ["hello"], "الميكانيكي ابو محمد", 9, '70922772', 6, "blacksmiths/Blacksmiths", GeoPoint(33,33));
-  var rami =  serviceProvider('حمرا', 29, 3.9, [6,5,9], "xx", true, ["hello"], "الميكانيكي ابو علي", 9, '71089778', 6, "blacksmiths/Blacksmiths", GeoPoint(33,33));
-  var sundus = serviceProvider('مار الياس', 29, 4.0, [0,1,3], "xx", true, ["hello"], "الميكانيكي سمير", 9, '78765234', 6, "blacksmiths/Blacksmiths", GeoPoint(33,33));
-  var nadeem =serviceProvider('حي السلم', 29, 2.0, [1,2,7], "xx", true, ["hello"], "الميكانيكي مازن", 9, '03123446', 6, "blacksmiths/Blacksmiths", GeoPoint(33,33));
-  List<serviceProvider> prof = [reem,rami,sundus,nadeem];
+  var reem = Servicer('راس بيروت', 29, 3.0, [1,5,10], "xx", true, ["hello"], "الميكانيكي ابو محمد", 9, '70922772', 6, "blacksmiths/Blacksmiths", GeoPoint(33,33));
+  var rami =  Servicer('حمرا', 29, 3.9, [6,5,9], "xx", true, ["hello"], "الميكانيكي ابو علي", 9, '71089778', 6, "blacksmiths/Blacksmiths", GeoPoint(33,33));
+  var sundus = Servicer('مار الياس', 29, 4.0, [0,1,3], "xx", true, ["hello"], "الميكانيكي سمير", 9, '78765234', 6, "blacksmiths/Blacksmiths", GeoPoint(33,33));
+  var nadeem =Servicer('حي السلم', 29, 2.0, [1,2,7], "xx", true, ["hello"], "الميكانيكي مازن", 9, '03123446', 6, "blacksmiths/Blacksmiths", GeoPoint(33,33));
+  List<Servicer> prof = [reem,rami,sundus,nadeem];
                          // List<serviceProvider> list = await fb.getServicers('blacksmiths/Blacksmiths');
                           Navigator.push(
             context, MaterialPageRoute(builder: (context) => Myapp(prof)));
