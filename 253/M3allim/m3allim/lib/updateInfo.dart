dc0256372a0c2dec14c247000290866ecf66d881
issa1618 <49068288+issa1618@users.noreply.github.com>
2019-04-07 22:16:53 +0300

    Update updateInfo.dart

--- 253/M3allim/m3allim/lib/updateInfo.dart
+++ 253/M3allim/m3allim/lib/updateInfo.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 //import 'package:firebase_auth/firebase_auth.dart';
 
-import './serviceProvider.dart';
+import './Servicer.dart';
 import './badge.dart';
 import 'package:flutter_rating/flutter_rating.dart';
 import './crud.dart';
 import './displayInfo.dart';
 import './displayServiceProvider.dart';
 
-serviceProvider sv;
+Servicer sv;
 double raty = 0;
 bool badge1 = false;
 bool badge2 = false;
