8812f5141952053f76a6c8a956273ac7702533ff
issa1618 <49068288+issa1618@users.noreply.github.com>
2019-04-08 18:30:47 +0300

    Update crud.dart

--- 253/M3allim/m3allim/lib/crud.dart
+++ 253/M3allim/m3allim/lib/crud.dart
@@ -7,68 +7,150 @@ import 'package:flutter/material.dart';
 
 
 class fb {
+
 //  p is object (provider or user) to be added to database
   static add(var p) {
-    String path = (p is User) ? "Users" : "Service Providers/" + p.prof_path;
+    String path = (p is User) ? "Users" : p.prof_path.split("/")[0];
     Firestore.instance.document(path + "/961-${p.phone}").setData(p.toJson());
   }
 
-//  categoryAndSub is like doctors/Cardiologists
-//  should return a list, it returns future
-  static getServicers(String categoryAndSub) async {
-    QuerySnapshot out = await Firestore.instance
-        .collection("Service Providers/" + categoryAndSub)
-        .getDocuments();
-    List<serviceProvider> lst = new List<serviceProvider>();
+//  category and subCategory should exactly math the names on firebase
+//  when called, the method where it is called should be async, and put "await" before the call
+//  sorting_criteria is either by "rating" or "distance"
+//  us is which user is requesting the list, helpful now and later
+  static getnServicers(String category, String subCategory, int n, String sorting_criteria, bool descending, User us) async {
+    CollectionReference cr = Firestore.instance.collection(category).where("prof_path", isEqualTo: "$category/$subCategory");
+    QuerySnapshot out = sorting_criteria == "distance"
+        ? await cr.limit(n).getDocuments()
+        : await cr.orderBy("average_rating", descending: descending).limit(n).getDocuments();
+    List<Servicer> lst = new List<Servicer>();
     for (DocumentSnapshot ds in out.documents)
-      lst.add(serviceProvider.fromSnapshot(ds));
+      lst.add(Servicer.fromSnapshot(ds));
+    if (sorting_criteria == "distance") {
+      for (Servicer sv in lst)
+        sv.distance = getDistance(sv, us);
+      lst.sort((Servicer a, Servicer b) => a.distance.compareTo(b.distance));
+    }
+    lst.sublist(min(lst.length, n));
     return lst;
+  }
+
+//  distance is in meters
+//  6371000 is earth's mean radius in meters
+  static getDistance(Servicer sv, User us) {
+    double svLat = sv.location.latitude, svLon = sv.location.longitude;
+    double usLat = us.location.latitude, usLon = us.location.longitude;
+    double latDif = (svLat - usLat), lonDif = (svLon - usLon);
+    double a = pow(sin(latDif * pi / 360), 2) +
+        cos(svLat) * cos(usLat) * pow(sin(lonDif * pi / 360), 2);
+    a = a < 0 ? (-1) * a : a;
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    return 6371000 * c;
   }
 
 //  p is object (provider or user) to be deleted from database
   static delete(var p) {
     String path = (p is User)
         ? "Users/961-${p.phone}"
-        : "Service Providers/${p.prof_path}/961-${p.phone}";
+        : "${p.prof_path.split("/")[0]}/961-${p.phone}";
     Firestore.instance.document(path).delete();
   }
 
-  static updateServicerStars(serviceProvider p, double num_stars) {
-    double new_num_stars =
-        (p.average_rating * p.number_rates + num_stars) / (p.number_rates + 1);
-    String path = "Service Providers/${p.prof_path}/961-${p.phone}";
-    Firestore.instance.document(path).updateData(
-        {"average_rating": new_num_stars, "number_rates": p.number_rates + 1});
-    return new_num_stars;
+  static updateServicerStars(Servicer sv, User us, int num_stars) {
+    String path = "${sv.prof_path.split("/")[0]}/961-${sv.phone}";
+    DocumentReference dr = Firestore.instance.document(path);
+    dr.get().then((out) {
+      sv.number_rates = out["number_rates"];
+      sv.average_rating =
+          (out["average_rating"] * sv.number_rates + num_stars) /
+              (sv.number_rates + 1);
+      sv.number_rates += 1;
+      dr.updateData({
+        "average_rating": sv.average_rating,
+        "number_rates": sv.number_rates
+      });
+    });
+    path = "Users/961-${us.phone}";
+    DocumentReference dr2 = Firestore.instance.document(path);
+    dr2.get().then((out) {
+      us.ratings = out["ratings"];
+      if (us.ratings[sv.phone] != null)
+        us.ratings[sv.phone]["stars"] = num_stars;
+      else
+        us.ratings.addAll({
+          sv.phone: {"badge": 0, "stars": num_stars}
+        });
+      dr2.updateData({"ratings": us.ratings});
+    });
   }
 
 //  badge_index 1 or 2 or 3
-  static incrementServicerBadges(serviceProvider p, int badge_index) {
-    String path = "Service Providers/${p.prof_path}/961-${p.phone}";
-    p.badges[badge_index] += 1;
-    Firestore.instance.document(path).updateData({"badges": p.badges});
+  static incrementServicerBadges(Servicer sv, User us, int badge_index) {
+    String path = "${sv.prof_path.split("/")[0]}/961-${sv.phone}";
+    sv.badges[badge_index - 1] += 1;
+    DocumentReference dr = Firestore.instance.document(path);
+    dr.get().then((out) {
+      List<int> badges = out["badges"].cast<int>();
+      badges[badge_index - 1]++;
+      dr.updateData({"badges": badges});
+    });
+    path = "Users/961-${us.phone}";
+    DocumentReference dr2 = Firestore.instance.document(path);
+    dr2.get().then((out) {
+      us.ratings = out["ratings"];
+      if (us.ratings[sv.phone] != null)
+        us.ratings[sv.phone]["badge"] = badge_index;
+      else
+        us.ratings.addAll({
+          sv.phone: {"badge": badge_index, "stars": 0}
+        });
+      dr2.updateData({"ratings": us.ratings});
+    });
   }
-
-  static decrementServicerBadges(serviceProvider p, int badge_index) {
-    String path = "Service Providers/${p.prof_path}/961-${p.phone}";
-    p.badges[badge_index] -= 1;
-    Firestore.instance.document(path).updateData({"badges": p.badges});
-  }
-
 
   static incrementCalls(var p) {
     String path = (p is User)
         ? "Users/961-${p.phone}"
-        : "Service Providers/${p.prof_path}/961-${p.phone}";
-    Firestore.instance.document(path).updateData({"calls": p.calls + 1});
+        : "${p.prof_path.split("/")[0]}/961-${p.phone}";
+    DocumentReference dr = Firestore.instance.document(path);
+    dr.get().then((out) {
+      p.calls = out["calls"];
+      dr.updateData({"calls": ++p.calls});
+    });
   }
 
-//  adding a review of a servicer to the user object
-  static addReview(User us, serviceProvider sp, int stars, int badge) {
-    us.ratings_badges.addAll({"961-${sp.phone}": {"badge": badge, "stars": stars}});
+  static addFavorite(User us, Servicer sv) {
     String path = "Users/961-${us.phone}";
-    Firestore.instance.document(path).updateData({"ratings_badges": us.ratings_badges});
+    DocumentReference dr = Firestore.instance.document(path);
+    dr.get().then((out) {
+      us.favorites.addAll({"961-${sv.phone}": sv.prof_path});
+      dr.updateData({"favorites": us.favorites});
+    });
   }
 
-  static addImage(serviceProvider p, Image img) {}
+  static getFavorites(User us) async {
+    String path = "Users/961-${us.phone}";
+    DocumentSnapshot qs = await Firestore.instance.document(path).get();
+    List<Servicer> lst = List();
+    Map<String, List<String>> m = Map();
+    for (String s in qs["favorites"].keys) {
+      if (m[qs["favorites"][s].split("/")[0]] != null)
+        m[qs["favorites"][s].split("/")[0]].add(s);
+      else
+        m.addAll({
+          qs["favorites"][s].split("/")[0]: [s]
+        });
+    }
+    for (String s in m.keys) {
+      for(String num in m[s]) {
+        QuerySnapshot qs = await Firestore.instance.collection(s).where(
+            "phone", isEqualTo: num.split("-")[1]).getDocuments();
+        if (qs.documents.length != 0)
+          lst.add(Servicer.fromSnapshot(qs.documents[0]));
+      }
+    }
+    return lst;
+  }
+
+  static addImage(Servicer sv, Image img) {}
 }
