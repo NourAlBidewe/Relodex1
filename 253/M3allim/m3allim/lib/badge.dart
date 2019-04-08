54e9e6d39944d6a02011724fca43a6e350499daf
issa1618 <49068288+issa1618@users.noreply.github.com>
2019-04-08 18:44:56 +0300

    Update badge.dart

--- 253/M3allim/m3allim/lib/badge.dart
+++ 253/M3allim/m3allim/lib/badge.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 //import 'package:cloud_firestore/cloud_firestore.dart';
-import './serviceProvider.dart';
+import './ServiceProvider.dart';
 import './crud.dart';
 
 
@@ -11,7 +11,7 @@ class Badge extends StatefulWidget {
 
   //bool badgeAlreadyPressed = false;
   int badgeNum;
-  serviceProvider sv;
+  ServiceProvider sv;
   String onClicked;
   String tag;
   String text;
@@ -47,7 +47,7 @@ class _Badge extends State<Badge> {
   String newImagePath;
   int badgeNum;
   bool isPressed = false;
-  serviceProvider s;
+  ServiceProvider s;
   String tag;
   String text;
 
