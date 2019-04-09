71b528d39899d452497d9a002d62b96365b58df2
fatimaissa7 <49286788+fatimaissa7@users.noreply.github.com>
2019-04-09 21:10:07 +0200

    Update Categories.dart

--- 253/M3allim/m3allim/lib/Categories.dart
+++ 253/M3allim/m3allim/lib/Categories.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 class Categories{
   String name;
   String imageName;
-  bool cat;
-  Categories(this.name, this.imageName, this.cat);
+  List<Categories> sub;
+
+  Categories(this.name, this.imageName, this.sub);
 
 }
