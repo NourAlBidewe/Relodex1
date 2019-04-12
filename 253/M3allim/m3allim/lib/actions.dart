247e78388bf2867a23f15925f6e13d67e04ab393
fatimaissa7 <49286788+fatimaissa7@users.noreply.github.com>
2019-04-12 13:17:34 +0200

    Update actions.dart

--- 253/M3allim/m3allim/lib/actions.dart
+++ 253/M3allim/m3allim/lib/actions.dart
@@ -1,39 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:m3allim/User.dart';
 import './actionButton.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import './crud.dart';
 import './ServiceProvider.dart';
+import './chooseRightActionButtons.dart';
 
 ServiceProvider sv;
+User u;
 
 class Actions extends StatefulWidget {
+
+  ServiceProvider sv;
+  User u;
+
   @override
   State<StatefulWidget> createState() {
-    return _Actions();
+    return _Actions(u, sv);
   }
 
-  Actions(ServiceProvider s) {
-    sv = s;
+  Actions(this.sv, this.u) {
+    
+    
+
   }
 }
 
 class _Actions extends State<Actions> {
+  
+  User u;
+  ServiceProvider sv;
+  _Actions(this.u, this.sv);
 
-
-  void  _launchcaller() async {
-    fb.incrementCalls(sv);
-    var url = "tel:" + sv.phone;
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  void hello(){
-
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,16 +41,17 @@ class _Actions extends State<Actions> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height / 6,
         padding: EdgeInsets.only(right: MediaQuery.of(context).size.width/20),
-          child: Row(mainAxisAlignment: MainAxisAlignment.center
-              ,
-              children: <Widget>[
-                ActionButton('btn1', Icons.edit, Colors.black38 ,Colors.white,'عدل', hello), //EDIT PAGE
-            ActionButton('btn3', Icons.person, Colors.black38,Colors.white, 'هذا انا',hello),
-            ActionButton('btn2', Icons.favorite, Colors.redAccent,Colors.white,'مفضل', hello),
+        child: chooseRightActionButtons(sv, u),
+          // child: Row(mainAxisAlignment: MainAxisAlignment.center
+          //     ,
+          //     children: <Widget>[
+                
+          //       //ActionButton('btn1', Icons.edit, Colors.black38 ,Colors.white,'عدل', hello), //EDIT PAGE
+          //   //ActionButton('btn3', Icons.person, Colors.black38,Colors.white, 'هذا انا',hello),
+          //   ActionButton('btn2', Icons.favorite, Colors.redAccent,Colors.white,'مفضل', booga),
+          //   ActionButton('btn9', Icons.phone, Colors.green ,Colors.white,'اتصل', _launchcaller),
 
-            ActionButton('btn1', Icons.phone, Colors.green ,Colors.white,'اتصل', _launchcaller),
-
-          ]),
+          // ]),
         );
   }
 }
