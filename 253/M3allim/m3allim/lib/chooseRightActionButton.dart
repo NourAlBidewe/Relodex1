ac7e6a76fe800c73aacd0420a6af3a5a06f6a94a
fatimaissa7 <49286788+fatimaissa7@users.noreply.github.com>
2019-04-15 17:44:36 +0200

    Update chooseRightActionButton.dart

--- 253/M3allim/m3allim/lib/chooseRightActionButton.dart
+++ 253/M3allim/m3allim/lib/chooseRightActionButton.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:m3allim/actionButton.dart';
 import 'package:m3allim/displayCards.dart';
+import 'package:m3allim/providerJobsCard.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import './Categories.dart';
@@ -11,8 +15,8 @@ import './crud.dart';
 class chooseRightActionButtons extends StatefulWidget {
   ServiceProvider sv;
   User u;
-
   
+
   chooseRightActionButtons(this.sv, this.u);
 
   @override
@@ -24,6 +28,9 @@ class chooseRightActionButtons extends StatefulWidget {
 class _chooseRightActionButtons extends State<chooseRightActionButtons> {
   ServiceProvider sv;
   User u;
+  bool changeBoarder = false;
+   
+  //static User us = new User(27, true, GeoPoint(33, 11), 'hello', 'x');
 
   _chooseRightActionButtons(this.sv, this.u);
 
@@ -37,43 +44,55 @@ class _chooseRightActionButtons extends State<chooseRightActionButtons> {
     }
   }
 
-// fb.getnServiceProviders('حداد', 'حداد', 3, 'distance', true, u),
-  Future<void> _neverSatisfied() async {
-  return showDialog<void>(
-    context: context,
-    barrierDismissible: false, // user must tap button!
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Rewind and remember'),
-        content: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-               // List<ServiceProvider> l = fb.getnServiceProviders('حداد', 'حداد', 3, 'distance', true, u),
-                //ServiceDisplay(listy,u),
-                //child: fb.getnServiceProviders('حداد', 'حداد', 3, 'distance', true, u),
-              
-            ],
-          )
-        ),
-        actions: <Widget>[
-          FlatButton(
-            child: Text('Regret'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
-    },
-  );
-}
+  displayOtherJobsByServiceProvider() async {
+    List<ServiceProvider> listy =
+        await fb.getnServiceProviders('سمكري', 'سمكري', 5, 'distance', false, u);
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          int counter = 0;
+          StreamController<String> controller =
+              StreamController<String>.broadcast();
+          return AlertDialog(
+            title: Center(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'اعمال اخرى ل',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    sv.name,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            content: Container(
+              width: MediaQuery.of(context).size.height / 2,
+              height: MediaQuery.of(context).size.height / 2,
+              child: ListView.builder(
+                itemCount: listy.length,
+                itemBuilder: (context, int) {
+                  return providerJobsCard(listy[int], u);
+                },
+              ),
+            ),
+          );
+        });
+  }
 
   void hello() {}
 
-  void booga() {
+  void addFavorite() {
     fb.addFavorite(u, sv);
-    print("HELLOOO");
-    print(u.favorites);
+    setState(() {
+      changeBoarder = true;
+      final snackBar = SnackBar(
+        content: Text('تمت الاضافة الى قائمة المفضلين',textDirection: TextDirection.rtl, ),
+      );
+      Scaffold.of(context).showSnackBar(snackBar);
+    });
   }
 
   @override
@@ -84,26 +103,33 @@ class _chooseRightActionButtons extends State<chooseRightActionButtons> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ActionButton('btn1', Icons.edit, Colors.black38, Colors.white,
-                'عدل', hello), //EDIT PAGE
-            ActionButton('btn3', Icons.remove_red_eye, Colors.black38,
-                Colors.white, 'اعمال اخرى له', hello),
+                'عدل', hello,false), //EDIT PAGE
+            ActionButton(
+                'btn3',
+                Icons.remove_red_eye,
+                Colors.black38,
+                Colors.white,
+                'اعمال اخرى',
+                displayOtherJobsByServiceProvider,false),
             ActionButton('btn2', Icons.favorite, Colors.redAccent, Colors.white,
-                'مفضل', booga),
+                'مفضل', addFavorite, changeBoarder),
             ActionButton('btn9', Icons.phone, Colors.green, Colors.white,
-                'اتصل', _launchcaller),
+                'اتصل', _launchcaller,false),
           ]);
     } else {
+      // print(prof_paths.length);
+      // print(imagess.length);
       print(sv.phone);
       print(u.phone);
       return Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ActionButton('btn3', Icons.remove_red_eye, Colors.black38,
-                Colors.white, 'انظر ايضا', _neverSatisfied),
+            ActionButton('btn10', Icons.work, Colors.black38,
+                Colors.white, 'اعمال اخرى', displayOtherJobsByServiceProvider,false),
             ActionButton('btn2', Icons.favorite, Colors.redAccent, Colors.white,
-                'مفضل', booga),
+                'مفضل', addFavorite,changeBoarder),
             ActionButton('btn9', Icons.phone, Colors.green, Colors.white,
-                'اتصل', _launchcaller),
+                'اتصل', _launchcaller,false),
           ]);
     }
   }
