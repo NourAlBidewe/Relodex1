558d5712aa9b55f750f5b2160e884fe661c2f328
fatimaissa7 <49286788+fatimaissa7@users.noreply.github.com>
2019-04-18 10:25:37 +0200

    Update displayCategoriesAndSubCategories.dart

--- 253/M3allim/m3allim/lib/displayCategoriesAndSubCategories.dart
+++ 253/M3allim/m3allim/lib/displayCategoriesAndSubCategories.dart
@@ -8,98 +8,90 @@ import 'package:m3allim/ServiceProvider.dart';
 import './displayOneCategoryCard.dart';
 import './Categories.dart';
 
-
 import './crud.dart';
 import 'DisplayServiceProvidersList.dart';
 
+// getUser() async {
+//   User u = User.fromSnapshot(await Firestore.instance.document("Users/961-33196876").get());
+//   return u;
+// }
 
-
-  // getUser() async {
-  //   User u = User.fromSnapshot(await Firestore.instance.document("Users/961-33196876").get());
-  //   return u;
-  // }
-
-
-
-
-class MyApp extends StatelessWidget{
-  String name;
+class MyApp extends StatelessWidget {
+  String name; //the name is tabeeb
   List<Categories> listCategories;
   MyApp(this.listCategories, this.name);
 
   @override
   Widget build(BuildContext context) {
-    
-    return MaterialApp(
-      home: _MyApp(listCategories,name));
+    return MaterialApp(home: _MyApp(listCategories, name));
   }
-  }
-
-
-
+}
 
 class _MyApp extends StatelessWidget {
-  User u = new User(20, false, GeoPoint(11, 44), "حسام", "14039903");
-  
-  
+  // User u = new User(20, false, [10, 11], "حسام", "10010551");
+  User u;
 
   List<Categories> categoriesList;
   String name;
-  _MyApp(this.categoriesList,this.name);
-
-  
-
+  _MyApp(this.categoriesList, this.name);
 
   @override
   Widget build(BuildContext context) {
-    final title = 'Categories';
-    return 
-       Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.deepPurple,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              color: Colors.white,
-              iconSize: 17.0,
-              onPressed: () {},
-            ),
-          ],
-        ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          padding: EdgeInsets.all(8.0),
-          crossAxisSpacing: 8.0,
-          mainAxisSpacing: 5.0,
-          children: categoriesList
-              .map((data) => GestureDetector(
-                    child: displayOneCard(data),
-                    onTap: () async {
-                      //User u = User.fromSnapshot(await Firestore.instance.document("Users/961-10105360").get());
-                      if (data.sub.length != 0) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => MyApp(data.sub,data.name)));
-                      } else {
-                        print('helloo');
-                          if(name.isEmpty){
-                            name = data.name;
-                          }
-                        List<ServiceProvider> prof =
-                            await fb.getnServiceProviders(
-                                name, data.name, 5, 'rating', true, u);
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context)  => DisplayServiceProvidersList(prof, u, name, data.name)));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            color: Colors.white,
+            iconSize: 17.0,
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(8.0),
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 5.0,
+        children: categoriesList
+            .map((data) => GestureDetector(
+                  child: displayOneCard(data),
+                  onTap: () async {
+                    u = User.fromSnapshot(await Firestore.instance.document("Users/961-10010551").get());
+                    if (data.sub.length != 0) {
+                      // print('FROM CATEGORY TO SUBCATEGORY');
+                      // print('category name: ' + name);
+                      // print('subCategory name: ' + data.name);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  MyApp(data.sub, data.name)));
+                    } else {
+                      String catName = '';
+                      //  print('FROM CATEGORY TO LIST');
+                      // print('category name: ' + name);
+                      // print('subCategory name: ' + data.name);
+                      if (name.isEmpty) {
+                        name = data.name;
                       }
-                    },
-                  ))
-              .toList(),
-        ),
-        bottomNavigationBar: bottomNavigator(),
-      
+                      List<ServiceProvider> prof =
+                          await fb.getnServiceProviders(
+                              name, data.name, 50, 'rating', true, u);
+                      catName = name;
+                      name = "";
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DisplayServiceProvidersList(
+                                  prof, u, catName, data.name)));
+                    }
+                  },
+                ))
+            .toList(),
+      ),
+      bottomNavigationBar: bottomNavigator(u),
     );
   }
 }
