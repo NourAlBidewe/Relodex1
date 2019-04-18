4cbea2b0b1943f97c14c6ac61b3f6599abe8050b
fatimaissa7 <49286788+fatimaissa7@users.noreply.github.com>
2019-04-18 10:24:29 +0200

    Update bottomNavigator.dart

--- 253/M3allim/m3allim/lib/bottomNavigator.dart
+++ 253/M3allim/m3allim/lib/bottomNavigator.dart
@@ -1,48 +1,67 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-
+import 'package:m3allim/ServiceProvider.dart';
+import 'package:m3allim/User.dart';
+import 'package:m3allim/displayFavorites.dart';
+import 'crud.dart';
 import 'addServiceProvider.dart';
+
 // import 'x.dart';
 // import './reemApp.dart';
-class bottomNavigator extends StatefulWidget{
+class bottomNavigator extends StatefulWidget {
+  User u;
+
+  bottomNavigator(this.u);
   @override
   State<StatefulWidget> createState() {
-    return _bottomNavigator();
+    return _bottomNavigator(u);
   }
-  
 }
 
-class _bottomNavigator extends State<bottomNavigator>{
+class _bottomNavigator extends State<bottomNavigator> {
+  User u;
+
+  _bottomNavigator(this.u);
+
   int indexy = 0;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return CurvedNavigationBar(
-        color: Colors.white,
-           initialIndex: 0,
-          animationDuration: Duration(milliseconds: 150),
+      color: Colors.white,
+      //  initialIndex: 0,
+      animationDuration: Duration(milliseconds: 150),
       backgroundColor: Colors.deepPurpleAccent,
       items: <Widget>[
-        Icon(Icons.home, size: 30, color: Colors.deepPurpleAccent,),
+        Icon(
+          Icons.home,
+          size: 30,
+          color: Colors.deepPurpleAccent,
+        ),
         Icon(Icons.add, size: 30, color: Colors.deepPurpleAccent),
-        //Icon(Icons.favorite, size: 30, color: Colors.white),
+        Icon(Icons.favorite, size: 30, color: Colors.deepPurpleAccent),
       ],
-      onTap: (index) {
-        
-          setState(() {
-            if(index ==1){
-             Navigator.push(context,
-              MaterialPageRoute(
-                  builder: (context) => MyApp()));
-            } 
-           
-            
-             });
+      onTap: (index) async {
+        u = User.fromSnapshot(await Firestore.instance.document("Users/961-10010551").get());
          
-
+        // setState(()  {
+         
+          if (index == 1) {
+            
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => MyApp(u)));
+          }
+          if (index == 2) {
+              List<ServiceProvider> listOfFavorites = await fb.getFavorites(u);
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        displayFavorites(listOfFavorites, u)));
+          }
+        // });
       },
     );
   }
-
 }
